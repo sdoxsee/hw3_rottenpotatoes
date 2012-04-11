5225bf0135ed60b9638fdf540ec0196474a1e424
Stephen Doxsee <stephen@doxsee.org>
2012-04-11 09:40:12 -0400

    Some final touches to movie steps

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,10 +4,6 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    # m = Movie.new(
-      # :title => movie[:title], 
-      # :rating => movie[:rating], 
-      # :release_date => movie[:release_date])
     m = Movie.new(movie)
     m.save!
   end
